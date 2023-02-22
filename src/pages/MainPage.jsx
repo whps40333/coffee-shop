490b8a94c243600cf03c3fa7fb4000c4b189cc63
whps40333 <whps40333@gmail.com>
2023-02-22 17:11:28 +0800

    adjest card wrapper

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,19 @@
 import Clrbutton from "../components/UI/Buttons/ClrButton";
 import Editbutton from "../components/UI/Buttons/EditButton";
 import Card from "../components/UI/Modals/Card";
+import classes from "../styles/pages/MainPage.module.css";
 
 function MainPage() {
   return (
     <>
-      <Card />
+      <div className={classes.wrapper}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
       <Editbutton>登出</Editbutton>
       <Clrbutton>編輯個人資料</Clrbutton>
       <p>this is Main page</p>;
