e65100705efce3fc5f92d5552bb05666a87f74c3
whps40333 <whps40333@gmail.com>
2023-02-22 16:59:11 +0800

    add clrButton,editbutton,card

--- src/components/UI/Buttons/EditButton.jsx
+++ src/components/UI/Buttons/EditButton.jsx
@@ -1,2 +1,15 @@
+import classes from "../../../styles/EditButton.module.css";
 
+const Editbutton = (props) => {
+  return (
+    <button
+      className={classes.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+};
 
+export default Editbutton;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,16 @@
+import Clrbutton from "../components/UI/Buttons/ClrButton";
+import Editbutton from "../components/UI/Buttons/EditButton";
+import Card from "../components/UI/Modals/Card";
+
 function MainPage() {
-  return <p>this is Main page</p>;
+  return (
+    <>
+      <Card />
+      <Editbutton>登出</Editbutton>
+      <Clrbutton>編輯個人資料</Clrbutton>
+      <p>this is Main page</p>;
+    </>
+  );
 }
 
 export default MainPage;
