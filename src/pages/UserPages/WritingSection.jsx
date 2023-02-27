568bb7d5c01b18efcea6057b8ef3d6c7088ff660
whps40333 <whps40333@gmail.com>
2023-02-27 23:35:24 +0800

    add writingSection

--- src/pages/UserPages/WritingSection.jsx
+++ src/pages/UserPages/WritingSection.jsx
@@ -1,5 +1,50 @@
+import React, { useRef } from "react";
+import classes from "../../styles/pages/UserPages/WritingSection.module.css";
+import Clrbutton from "../../components/UI/Buttons/ClrButton";
+
 function WritingSection() {
-  return <p>this is Writing section</p>;
+  const titleRef = useRef("");
+  const visitDateRef = useRef("");
+  const commentRef = useRef("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const restaurant = {
+      title: titleRef.current.value,
+      visitDate: visitDateRef.current.value,
+      comment: commentRef.current.value,
+    };
+
+    console.log(restaurant);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor="title"></label>
+        <input type="text" id="title" ref={titleRef} placeholder="餐廳名稱" />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="Date"></label>
+        <input
+          type="text"
+          id="date"
+          ref={visitDateRef}
+          placeholder="2022-02-27"
+        />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="comment"></label>
+        <textarea
+          type="text"
+          id="comment"
+          ref={commentRef}
+          placeholder="用餐回饋"
+        ></textarea>
+      </div>
+      <Clrbutton type="submit">提交</Clrbutton>
+    </form>
+  );
 }
 
 export default WritingSection;
